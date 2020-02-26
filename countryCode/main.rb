393cdb21f8060e8a53a683574566fba3018f8a05
Somnath Paul <Somnath4x@gmail.com>
2020-02-26 22:12:47 +0530

    Add: Opp

--- countryCode/main.rb
+++ countryCode/main.rb
@@ -1,26 +1,26 @@
 # hash table with country name and code
 countryCode = [
-    {country: "afghanistan", code: "335", popullation:"20.65 Billion"},
-    {country: "albania", code: "213", popullation:"12.8 Billion"},
-    {country: "algeria", code: "1-684", popullation: "215.7 Billion"},
-    {country: "american Samoa", code: "376", popullation: "462.2 Million"},
-    {country: "andorra", code: "244", popullation: "4.8 Billion" },
-    {country: "angola", code: "1-164", popullation: "124 Billion" },
-    {country: "anguilla", code: "672", popullation: "175.4 Million"},
-    {country: "antarctica", code: "1-264", popullation: "Null" },
-    {country: "antigua and Barbuda", code: "672", popullation: "1.22 Billion" },
-    {country: "argentina", code: "1-268", popullation: "484.6 Billion" },
-    {country: "armenia", code: "54", popullation: "	10.44 Billion" },
-    {country: "aruba", code: "297", popullation:"10.44 Billion"}
+    {country: "afghanistan", code: "335", population:"20.65 Billion"},
+    {country: "albania", code: "213", population:"12.8 Billion"},
+    {country: "algeria", code: "1-684", population: "215.7 Billion"},
+    {country: "american Samoa", code: "376", population: "462.2 Million"},
+    {country: "andorra", code: "244", population: "4.8 Billion" },
+    {country: "angola", code: "1-164", population: "124 Billion" },
+    {country: "anguilla", code: "672", population: "175.4 Million"},
+    {country: "antarctica", code: "1-264", population: "Null" },
+    {country: "antigua and Barbuda", code: "672", population: "1.22 Billion" },
+    {country: "argentina", code: "1-268", population: "484.6 Billion" },
+    {country: "armenia", code: "54", population: "	10.44 Billion" },
+    {country: "aruba", code: "297", population:"10.44 Billion"}
 
 
 ]
 
 # custom method to get country code form country name
  def getCountryCode(country,code)
-     code.each do |data, code, popullation|
+     code.each do |data, code, population|
         if data[:country] == country
-            return "Country: #{data[:country]}, code: #{data[:code]}, popullation: #{data[:popullation]}"
+            return "Country: #{data[:country]}, Code: #{data[:code]}, Population: #{data[:population]}"
         end
     end 
     return "Wrong Input"   
@@ -29,6 +29,7 @@ countryCode = [
 
 puts
 puts "Welcome to country code finder!"
+puts "-"*30
 puts
 puts "Want to find country code?"
 puts "Type Y to preceed or type anything to quit"
---
+++ oop/main.rb
@@ -0,0 +1,12 @@
+class Course
+    # variable in class is always written with @
+    @courseName
+    @courseLength
+    @courseType
+    @courseDepartment
+    @courseNeed
+end
+
+# create new object
+
+ruby = Course.new
