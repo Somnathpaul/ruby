75b3225d5d31e4a37dc0180c0e77731bea6bd68a
Somnath Paul <Somnath4x@gmail.com>
2020-02-26 18:45:23 +0530

    Modified

--- auth/main.rb
+++ auth/main.rb
@@ -5,30 +5,38 @@ db = [
     {name: "go", password: "go12345"},
     {name: "js", password: "js12345"}
 ]
+# auth method
+def auth(userName, password, data)
+    data.each do |user|
+        if user[:name] == userName && user[:password] == password
+            return user # will return object
+        end
+end
+return "Credentials were not correct"
+end
+
 puts
 puts "Welcome to the cli auth"
 puts
 
-attemp = 4
+attemp = 0
 while attemp < 5
-    puts "Press L to login or Q to quit"
+    puts "Press any key to login or Q to quit"
     input = gets.chomp.downcase 
     if input == "q"
         break 
     
-else 
+else
+        attemp = attemp + 1
         puts "Enter Username"
         userName = gets.chomp
         puts "Enter Password"
         password = gets.chomp
-        db.each do |user|
-            if user[:name] == userName && user[:password] == password
-                puts user
-                break
-            else
-                puts "Credentials were not correct"
-                break
-            end
-end
+        # call the method, pass parameters: username , password and database
+        output = auth(userName, password, db) # store the return in a variable
+        puts output # print the return value
+        
 end  
 end  
+puts
+puts "You have reached too many attemps. Try later!" if attemp = 4
